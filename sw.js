d593061b2ce328e5f77ac46f40ffa93e35493390
085_Teuzin <158859898+085Teuzin@users.noreply.github.com>
2026-02-15 10:57:51 -0300

    Atualizar o sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,29 @@
-const CACHE_NAME = 'orange-portable-v1';
+const CACHE_NAME = 'orange-portable-v2';
 const assets = [
   './',
   './index.html',
+  './database.js',
   './manifest.json',
   './icon.png'
 ];
+
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(assets);
     })
   );
+  self.skipWaiting();
 });
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)));
+    })
+  );
+});
+
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
